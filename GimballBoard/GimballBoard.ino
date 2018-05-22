c9a2c6191b82425ae2f9cbc877e4bedc398125d6
Judah Schad <judah_schad@yaskawa.com>
2018-05-22 13:13:21 -0500

    Comments/refactor for Andrew code review

--- GimballBoard/GimballBoard.ino
+++ GimballBoard/GimballBoard.ino
@@ -4,8 +4,9 @@
  * Used with 2 Drill Board Rev 1 boosters on a single TIVA
  * Andrew Van Horn
  * 
- * Controls 2 open-loop motors Pan, Tilt), 1 closed loop motor with two limit switches for position and an encoder (Mast up/down) 
- *          1 servo (roll), Camera Zoom (Hardware PWM write from a limit switch channel), Camera Focus (Hardware PEM write from a limit switch channel)
+ * Controls 2 open-loop motors: Pan, Tilt
+ *          1 closed loop motor with two limit switches for position limits and one encoder: Mast (Up/Down)
+ *          3 Servos :Roll, Camera Zoom, Camera Focus 
  */
 
 #include "RoveWare.h"
@@ -47,15 +48,11 @@ const uint8_t ROLL_SERVO_PIN = PK_1;
 const uint8_t CAMERA_ZOOM_PIN  = PK_0; //Camera Zoom  runs off X7 Limit Switch 2
 const uint8_t CAMERA_FOCUS_PIN = PB_5; //Camera Focus runs off X7 Limit Switch 3
 
-//Zoom Setup//////////////////////////
-const int CAMERA_SHORT_SIGNAL  = 1000;
-const int CAMERA_MIDDLE_SIGNAL = 1500;
-const int CAMERA_LONG_SIGNAL   = 2000;
-
 //Mast Encoder Setup///////////////////////
 const uint16_t MAST_UP_SERVO_VALUE    = 1;
 const uint16_t MAST_DOWN_SERVO_VALUE  = 1;
 const int      MAST_CLOSED_LOOP_SPEED = 300; 
+
 uint16_t       mast_move_to_position  = 0;
 
 ////////////////////
@@ -65,19 +62,30 @@ uint16_t       mast_move_to_position  = 0;
 RoveCommEthernetUdp  RoveComm;
 RoveWatchdog         Watchdog;
 
-//Read Variables////
-uint16_t data_id; 
-size_t   data_size; 
-uint8_t  data[2];
-
 //////////////////////
 RoveVnh5019 PanMotor;
 RoveVnh5019 TiltMotor;
 RoveVnh5019 MastMotor;
+
 Servo       RollServo;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// notice in https://github.com/energia/Energia/blob/master/hardware/lm4f/libraries/Servo/Servo.h 
+// #define MIN_SERVO_PULSE_WIDTH       544
+// #define DEFAULT_SERVO_PULSE_WIDTH   1500 
+// #define MAX_SERVO_PULSE_WIDTH       2400 
+// unsigned int attach(unsigned int pin, int min = MIN_SERVO_PULSE_WIDTH, int max = MAX_SERVO_PULSE_WIDTH);
+
+// effectively, the cable is implementing these as a 'continuous servo' which looks like 1000:1500:2000 in micros for reverse_speed:stop:forward_speed_) instead of a 'position servo' (0 to 180 position degrees)
+
+#define RC_CAMERA_MAX_REVERSE    1000
+#define RC_CAMERA_ZERO           1500
+#define RC_CAMERA_MAX_FORWARD    2000 
+
+Servo       CameraZoom;
+Servo       CameraFocus;
+
 void estop(); // Watchdog Estop Function
-void generateCameraSignal(int amt, int pin);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() 
@@ -92,14 +100,15 @@ void setup()
   TiltMotor.begin(TILT_INA_PIN, TILT_INB_PIN, TILT_PWM_PIN);   
   MastMotor.begin(MAST_INA_PIN, MAST_INB_PIN, MAST_PWM_PIN);  
   
-  RollServo.attach(ROLL_SERVO_PIN);
+  RollServo.attach(  ROLL_SERVO_PIN);
+  CameraZoom.attach( CAMERA_ZOOM_PIN,  RC_CAMERA_MAX_REVERSE, RC_CAMERA_MAX_FORWARD); 
+  CameraFocus.attach(CAMERA_FOCUS_PIN, RC_CAMERA_MAX_REVERSE, RC_CAMERA_MAX_FORWARD); 
   delay(10);
-
-  pinMode(CAMERA_ZOOM_PIN,  OUTPUT);
-  pinMode(CAMERA_FOCUS_PIN, OUTPUT);
-
-  digitalWrite(CAMERA_ZOOM_PIN,  0); 
-  digitalWrite(CAMERA_FOCUS_PIN, 0);
+  
+  // I'm assuming they mount the servo centered at 90 (so that it sweep -90 degrees clockwise to 0 position, and +90 degrees to counter clockwise 180 position)
+  RollServo.write(90);
+  CameraZoom.writeMicroseconds( RC_CAMERA_ZERO); 
+  CameraFocus.writeMicroseconds(RC_CAMERA_ZERO);
   
   pinMode(MAST_ENCODER_PIN, INPUT);
   delay(10);
@@ -110,13 +119,18 @@ void setup()
 ///////////////////////////////////////////////////////////////////
 void loop()
 {   
+  uint16_t data_id; 
+  size_t   data_size; 
+  int8_t   data[2];
+
   RoveComm.read(&data_id, &data_size, data);
 
   switch(data_id)
   {
     case GIMBAL_PAN:
     {
-      int pan_speed = *(int16_t*)(data);       
+      int pan_speed = *(int16_t*)(data); 
+      pan_speed  = map(pan_speed,  RED_MAX_REVERSE, RED_MAX_FORWARD, VNH5019_MAX_REVERSE, VNH5019_MAX_FORWARD);       
       PanMotor.drive(pan_speed);       
       Watchdog.clear();
       break;
@@ -124,7 +138,8 @@ void loop()
 
     case GIMBAL_TILT:
     {
-      int tilt_speed = *(int16_t*)(data);       
+      int tilt_speed = *(int16_t*)(data); 
+      tilt_speed  = map(tilt_speed,  RED_MAX_REVERSE, RED_MAX_FORWARD, VNH5019_MAX_REVERSE, VNH5019_MAX_FORWARD);      
       TiltMotor.drive(tilt_speed);       
       Watchdog.clear();
       break;
@@ -132,7 +147,9 @@ void loop()
 
     case GIMBAL_ROLL:
     {
-      int servo_position = data[0];       
+      // I don't see any reason to special case a continuous position command from RED, I would advise Skelton uses -1000 for full clockwise rotation and 1000 for full counter clockwise
+      int servo_position = *(int16_t*)(data); 
+      servo_position  = map(servo_position,  RED_MAX_REVERSE, RED_MAX_FORWARD, 0, 180 );  	  
       RollServo.write(servo_position);       
       Watchdog.clear();
       break;
@@ -140,46 +157,50 @@ void loop()
 
     case CAMERA1_COMMAND:
     {
-      int command = data[0];      
-      switch(command)
-        {
-          case CAMERA_STOP:
-          {
-            generateCameraSignal(CAMERA_MIDDLE_SIGNAL, CAMERA_ZOOM_PIN);
-            generateCameraSignal(CAMERA_MIDDLE_SIGNAL, CAMERA_FOCUS_PIN);
-            break;
-          }
+      // I don't see any reason to special case a continuous zoom command from RED, I would advise Skelton uses -1000 for full zoom/focus out and 1000 for full zoom/focus in
+      int camera_command_data = *(int16_t*)(data);  
+	  camera_command_data  = map(camera_command_data,  RED_MAX_REVERSE, RED_MAX_FORWARD, RC_CAMERA_MAX_REVERSE, RC_CAMERA_MAX_FORWARD);  
+      switch(camera_command_data)
+      {
+        case CAMERA_STOP:
+        {			
+          CameraZoom.write(camera_command_data);
+          break;
+        }
   
-          case CAMERA_ZOOM_IN:
-          {
-            generateCameraSignal(CAMERA_LONG_SIGNAL, CAMERA_ZOOM_PIN);
-            break;
-          }
+        case CAMERA_ZOOM_IN:
+        {
+          CameraZoom.write(camera_command_data);
+          break;
+        }
 
-          case CAMERA_ZOOM_OUT:
-          {
-            generateCameraSignal(CAMERA_SHORT_SIGNAL, CAMERA_ZOOM_PIN);
-            break;
-          }
+        case CAMERA_ZOOM_OUT:
+        {
+          CameraZoom.write(camera_command_data);
+          break;
+        }
         
-          case CAMERA_FOCUS_IN:
-          {
-            generateCameraSignal(CAMERA_LONG_SIGNAL, CAMERA_ZOOM_PIN);
-            break;
-          }
+        case CAMERA_FOCUS_IN:
+        {
+          CameraFocus.write(camera_command_data);
+          break;
+        }
           
-          case CAMERA_FOCUS_OUT:
-          {
-            generateCameraSignal(CAMERA_SHORT_SIGNAL, CAMERA_ZOOM_PIN);
-            break;
-          }
-        }//End Case Camera Command
+        case CAMERA_FOCUS_OUT:
+        {
+          CameraFocus.write(camera_command_data);
+          break;
+        }
         Watchdog.clear(); 
+		
+		default: // always add a default
+		  break;
+	  }
     }  
     
     case MAST_MOVE_OPEN_LOOP:
     {
-      if(
+      //if( Todo?...
       int mast_speed = *(int16_t*)(data);       
       MastMotor.drive(mast_speed);  
       mast_move_to_position = 0;     
@@ -187,7 +208,7 @@ void loop()
       break;
     }
 
-   case MAST_MOVE_TO_POSITION://ToDo once I have a spec
+   case MAST_MOVE_TO_POSITION: // Todo once I have a spec
     {    
       mast_move_to_position = data+1
       Watchdog.clear();
@@ -201,13 +222,25 @@ void loop()
   ////////////////////////////////////////
   //     Mast Closed Loop Functions     //
   ////////////////////////////////////////
-
-  if((analogRead(MAST_ENCODER_PIN) >= MAST_UP_SERVO_VALUE) || (analogRead(MAST_ENCODER_PIN) <= MAST_DOWN_SERVO_VALUE)) MastMotor.brake(0);
   
-  if(mast_move_to_position)
+  // This part is what we should talk through, I wasn't aware we get encoders?
+
+  if(  (analogRead(MAST_ENCODER_PIN) >= MAST_UP_SERVO_VALUE  ) 
+    || (analogRead(MAST_ENCODER_PIN) <= MAST_DOWN_SERVO_VALUE)) // up to... but I like prettyfying my || and &&
   {
-    if(mast_move_to_position = 1) MastMotor.drive(-MAST_CLOSED_LOOP_SPEED);
-    if(mast_move_to_position = 2) MastMotor.drive( MAST_CLOSED_LOOP_SPEED);
+	MastMotor.brake(0);  // I'm still pushing for brackets on ALL if's....
+	
+  } else if(mast_move_to_position) // I think you would have immediately started driving even after you braked?
+  {
+    if(mast_move_to_position = 1) 
+    {
+      MastMotor.drive(-MAST_CLOSED_LOOP_SPEED);
+	}
+	
+    if(mast_move_to_position = 2) 
+	{  
+      MastMotor.drive( MAST_CLOSED_LOOP_SPEED);
+	}
   }
 }
 
@@ -218,18 +251,5 @@ void estop()
   TiltMotor.brake(0);
   MastMotor.brake(0); 
   return;     
-}
-
-//////////////////////////////////////////
-void generateCameraSignal(int amt, int pin)
-{
-  for(int i = 0; i<5l; i++)
-  {
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(amt);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(20000-amt);
-  }
-  return;
 }
 
